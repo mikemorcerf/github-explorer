85c4d0ddf659aa8fc9b5d06a2a76716cec009db8
Michael Moura <mikemorcerf@gmail.com>
2020-04-20 20:13:13 -0400

    style Dashboard component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Github repositories</Title>
+
+      <Form>
+        <input placeholder="Type the repository's name" />
+        <button type="submit">Search</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/16601136?s=460&u=cc113eafff21220b4a2ef9c77d0456b6ff6175ce&v=4"
+            alt="Mike bolado"
+          />
+          <div>
+            <strong>mikemorcerf/repository</strong>
+            <p>Easy to use component in ReactJS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/16601136?s=460&u=cc113eafff21220b4a2ef9c77d0456b6ff6175ce&v=4"
+            alt="Mike bolado"
+          />
+          <div>
+            <strong>mikemorcerf/repository</strong>
+            <p>Easy to use component in ReactJS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars3.githubusercontent.com/u/16601136?s=460&u=cc113eafff21220b4a2ef9c77d0456b6ff6175ce&v=4"
+            alt="Mike bolado"
+          />
+          <div>
+            <strong>mikemorcerf/repository</strong>
+            <p>Easy to use component in ReactJS</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
